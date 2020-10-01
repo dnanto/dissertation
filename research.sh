46744431c68deb255aa4723433cf1f84f3f03dd6
dnanto <dnegron2@gmu.edu>
2020-10-01 01:34:19 -0400

    update metadata, use glsearch instead of BLAST+

--- research.sh
+++ research.sh
@@ -5,32 +5,18 @@
 blastdb="blast"
 mkdir -p "$blastdb"
 
-awk 'NR > 1 { print $1 }' genome.tsv > "$blastdb/genome.ssv"
-
 awk 'NR > 1 { print $1 }' genome.tsv | \
-xargs -L 250 | \
-tr ' ' ',' | \
-xargs -L 1 efetch -db nuccore -format fasta -id | \
-makeblastdb \
-  -input_type fasta -dbtype nucl -title genome \
-  -parse_seqids -hash_index -out "$blastdb/genome" -blastdb_version 5 \
-  -logfile "$blastdb/genome.log" -taxid_map "$blastdb/genome.ssv"
+  xargs -L 250 | tr ' ' ',' | \
+  xargs -L 1 efetch -db nuccore -format fasta -id > "$blastdb/genome.fasta"
 
 # prototype
 
-awk 'NR > 1 && $0 !~ /^#/ {print $0}' query.tsv | \
-while IFS=$'\t' read -r id seq_start seq_stop strand species region
+awk 'NR > 1 && $0 !~ /^#/ && $6 != "gen" {print $0}' query.tsv | \
+while IFS=$'\t' read -r id seq_start seq_stop strand species region protein_id
 do
   root="${species}-${region}"
   mkdir -p "$root"
   efetch -db nuccore -id "$id" -seq_start "$seq_start" -seq_stop "$seq_stop" -strand "$strand" -format gb > "$root/ref.gb"
-  efetch -db nuccore -id "$id" -seq_start "$seq_start" -seq_stop "$seq_stop" -strand "$strand" -format fasta >  "$root/ref.fasta"
-  blastn \
-    -task blastn \
-    -db "$blastdb/genome" \
-    -query "$root/ref.fasta" \
-    -outfmt "7 std qlen sstrand" \
-    -num_alignments 100000 \
-    -num_threads 16 \
-    -out "$root/hits.tsv"
+  efetch -db nuccore -id "$id" -seq_start "$seq_start" -seq_stop "$seq_stop" -strand "$strand" -format fasta > "$root/ref.fasta"
+  glsearch36 -T 16 -m 8C "$root/ref.fasta" "$blastdb/genome.fasta" > "$root/hits.tsv"
 done
