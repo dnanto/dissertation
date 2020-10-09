78d718faea538a0725a2b276caf2932eef0350d8
dnanto <dnegron2@gmu.edu>
2020-10-09 14:42:17 -0400

    high-resolution location and virus type

--- workflow/scripts/neogeocoor.R
+++ workflow/scripts/neogeocoor.R
@@ -4,43 +4,38 @@ suppressPackageStartupMessages({
   library(readr)
   library(dplyr)
   library(tidyr)
+  library(tibble)
   library(stringr)
 })
+
+codes <- c("country_code", paste0("admin", 1:4, "_code"))
 
 df.meta <- read_tsv(
   snakemake@input[[1]],
   col_types = cols(.default = "c"),
-  na = "?"
+  na = ""
 )
 
+# subset and order by feature code priority
 levels <- as.vector(do.call(c, neogeonames::akfc))
 geoname <- select(
   neogeonames::geoname,
-  feature_code, country_code, paste0("admin", 1:4, "_code"), latitude, longitude
+  feature_code, codes, latitude, longitude
 )
 geoname <- geoname[order(ordered(geoname$feature_code, levels = levels)), ]
 
-codes1 <- c("country", paste0("div", 1:4))
-codes2 <- c("country_code", paste0("admin", 1:4, "_code"))
-
-lapply(1:5, function(idx) {
-  bind_rows(
-    select(df.meta, codes1[1:idx]) %>%
-      filter(!is.na(.[idx])) %>%
-      distinct() %>%
-      apply(1, function(row) {
-        if (!all(row==F)) {
-          df <- data.frame(t(row[(i <- which(!is.na(row)))]))
-          left_join(setNames(df, codes2[i]), geoname, by = codes2[i])[1, ] %>%
-            replace(is.na(.), "?") %>%
-            mutate(
-              type = codes1[idx],
-              name = apply(.[codes2[1:idx]], 1, paste0, collapse = ".")
-            )
-        }
-      })
-  )
-}) %>%
+separate(df.meta, "location", codes, "\\.", extra = "drop", fill = "right", remove = F) %>%
+  select(location, all_of(codes)) %>%
+  distinct() %>%
+  apply(1, function(row) {
+    Filter(Negate(is.na), row) %>%
+      .[. != "?"] %>%
+      t() %>%
+      data.frame() %>%
+      left_join(geoname, by = tail(names(.), -1)) %>%
+      slice_head(n = 1)
+  }) %>%
   bind_rows() %>%
-  select(type, name, latitude, longitude) %>%
+  mutate(type = "location") %>%
+  select(type, location, latitude, longitude) %>%
   write_tsv(snakemake@output[[1]], col_names = F)
